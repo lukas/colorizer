ac9cb62ebfc7a4ba9d466a97aafdbfafa3161430
lukas <lukeab@gmail.com>
2019-04-01 05:30:00 +0000

    new loss function

--- color.py
+++ color.py
@@ -11,6 +11,8 @@ import os
 from PIL import Image
 import numpy as np
 import cv2
+from keras import backend as K
+from skimage import io, color
 
 run = wandb.init()
 config = run.config
@@ -44,7 +46,9 @@ def my_generator(batch_size, img_dir):
               color_images[i] = np.array(img)
               bw_images[i] = np.array(img.convert('L'))
         yield (bw_images, color_images)
-        counter += batch_size 
+        counter += batch_size
+
+
 
 model = Sequential()
 model.add(Reshape((config.height,config.width,1), input_shape=(config.height,config.width)))
@@ -54,13 +58,20 @@ model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(3, (3, 3), activation='relu', padding='same'))
 
+def perceptual_distance(y_true, y_pred):
+    rmean = ( y_true[:,:,:,0] + y_pred[:,:,:,0] ) / 2;
+    r = y_true[:,:,:,0] - y_pred[:,:,:,0]
+    g = y_true[:,:,:,1] - y_pred[:,:,:,1]
+    b = y_true[:,:,:,2] - y_pred[:,:,:,2]
+    
+    return K.mean(K.sqrt((((512+rmean)*r*r)/256) + 4*g*g + (((767-rmean)*b*b)/256)));
 
-model.compile(optimizer='adam', loss='mse')
+model.compile(optimizer='adam', loss='mse', metrics=[perceptual_distance])
 
 (val_bw_images, val_color_images) = next(my_generator(145, val_dir))
 
 model.fit_generator( my_generator(config.batch_size, train_dir),
-                     steps_per_epoch=20,
+                     steps_per_epoch=2,
                      epochs=config.num_epochs, callbacks=[WandbCallback(data_type='image', predictions=16)],
                      validation_data=(val_bw_images, val_color_images))
 
